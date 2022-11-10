9ce96c9fb65d979c5c27f692d179ef4cc6fed3d9
CH <ceherediafi@gmail.com>
2022-11-09 23:35:29 -0500

    Primer commit CH

--- src/main/java/equipo1/registraduria/seguridad/Controladores/ControladorRol.java
+++ src/main/java/equipo1/registraduria/seguridad/Controladores/ControladorRol.java
@@ -1,5 +1,58 @@
 package equipo1.registraduria.seguridad.Controladores;
+import equipo1.registraduria.seguridad.Modelos.Rol;
+import equipo1.registraduria.seguridad.Repositorios.RepositorioRol;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+@CrossOrigin
+@RestController
+@RequestMapping("/roles")
 
 public class ControladorRol {
-
+    @Autowired
+    private RepositorioRol miRepositorioRol;
+    @GetMapping("")
+    public List<Rol> index(){
+        return this.miRepositorioRol.findAll();
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Rol create(@RequestBody Rol infoUsuario){
+        infoUsuario.set_contrasena(convertirSHA256(infoUsuario.get_contrasena()));
+        return this.miRepositorioRol.save(infoUsuario);
+    }
+    @GetMapping("{id}")
+    public Rol show(@PathVariable String id){
+        Rol rolActual=this.miRepositorioRol
+                .findById(id)
+                .orElse(null);
+        return rolActual;
+    }
+    @PutMapping("{id}")
+    public Rol update(@PathVariable String id,@RequestBody Rol infoRol){
+        Rol rolActual=this.miRepositorioRol
+                .findById(id)
+                .orElse(null);
+        if (rolActual!=null){
+            rolActual.set_nombre(infoRol.get_nombre());
+            rolActual.set_descripcion(infoRol.get_descripcion());
+            return this.miRepositorioRol.save(rolActual);
+        }else{
+            return null;
+        }
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        Rol usuarioAEliminar=this.miRepositorioRol
+                .findById(id)
+                .orElse(null);
+        if (usuarioAEliminar!=null){
+            this.miRepositorioRol.delete(usuarioAEliminar);
+        }
+    }
 }
+
--- src/main/java/equipo1/registraduria/seguridad/Repositorios/RepositorioRol.java
+++ src/main/java/equipo1/registraduria/seguridad/Repositorios/RepositorioRol.java
@@ -1,5 +1,6 @@
 package equipo1.registraduria.seguridad.Repositorios;
-
+import equipo1.registraduria.seguridad.Repositorios.RepositorioRol;
+import org.springframework.data.mongodb.repository.MongoRepository;
 public interface RepositorioRol {
 
 }
