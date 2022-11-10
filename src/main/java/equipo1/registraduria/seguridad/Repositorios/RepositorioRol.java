292d21a83b65ec1bb2834b1165a514bea93556e5
OscarMadrid <oscar.madrid2359@gmail.com>
2022-11-09 23:24:38 -0500

    Cada uno deberá crear sus tablas con el presente proyecto

--- src/main/java/equipo1/registraduria/seguridad/Repositorios/RepositorioRol.java
+++ src/main/java/equipo1/registraduria/seguridad/Repositorios/RepositorioRol.java
@@ -1,7 +1,5 @@
 package equipo1.registraduria.seguridad.Repositorios;
-import equipo1.registraduria.seguridad.Modelos.Rol;
-import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RepositorioRol extends MongoRepository<Rol,String>{
+public interface RepositorioRol {
 
 }
--- src/main/java/equipo1/registraduria/seguridad/Repositorios/RepositorioUsuario.java
+++ src/main/java/equipo1/registraduria/seguridad/Repositorios/RepositorioUsuario.java
@@ -1,10 +1,6 @@
 package equipo1.registraduria.seguridad.Repositorios;
 
-import org.springframework.data.mongodb.repository.Query;
-import equipo1.registraduria.seguridad.Modelos.Usuario;
-import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RepositorioUsuario extends MongoRepository<Usuario,String>{
-    @Query("{'correo':?0}")
-    public Usuario getUserByEmail(String correo);
+public interface RepositorioUsuario {
+
 }
