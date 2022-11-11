18894b5a3e2529fac90518a110bc820658c1cbf9
OscarMadrid <oscar.madrid2359@gmail.com>
2022-11-11 18:20:43 -0500

    Modificación del RepositorioPermiso

--- src/main/java/equipo1/registraduria/seguridad/Repositorios/RepositorioPermiso.java
+++ src/main/java/equipo1/registraduria/seguridad/Repositorios/RepositorioPermiso.java
@@ -1,10 +1,10 @@
 package equipo1.registraduria.seguridad.Repositorios;
 import equipo1.registraduria.seguridad.Modelos.Permiso;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface RepositorioPermiso extends MongoRepository<Permiso,String> {
-
+    @Query("{'url':?0,'metodo':?1}")
+    Permiso getPermiso(String url, String metodo);
 
 }
