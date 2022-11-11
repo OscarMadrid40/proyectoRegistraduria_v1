05e6fd3137c6238a09c7457f2049af04ddf5978a
DEDE99 <quinoness.duran@gmail.com>
2022-11-11 14:43:51 -0500

    Modelo usuario

--- src/main/java/equipo1/registraduria/seguridad/Modelos/Usuario.java
+++ src/main/java/equipo1/registraduria/seguridad/Modelos/Usuario.java
@@ -1,5 +1,89 @@
 package equipo1.registraduria.seguridad.Modelos;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@Document
 public class Usuario {
 
+    @Id
+    private String _id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private  Number cedula;
+
+    private String seudonimo;
+
+    private String correo;
+
+    private String contrasena;
+
+    @DBRef
+    private Rol rol;
+
+    public Usuario(String nombre, String apellido, Number cedula, String seudonimo, String correo, String contrasena) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.seudonimo = seudonimo;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Number getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(Number cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getSeudonimo() {
+        return seudonimo;
+    }
+
+    public void setSeudonimo(String seudonimo) {
+        this.seudonimo = seudonimo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 }
