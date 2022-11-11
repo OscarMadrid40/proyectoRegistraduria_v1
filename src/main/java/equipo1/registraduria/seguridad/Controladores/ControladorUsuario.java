73ed14fc69452c65806c53f6ff269942da063936
ferbar007 <ferbar007@gmail.com>
2022-11-11 17:57:43 -0500

    Repositorio usuario FB

--- src/main/java/equipo1/registraduria/seguridad/Controladores/ControladorUsuario.java
+++ src/main/java/equipo1/registraduria/seguridad/Controladores/ControladorUsuario.java
@@ -6,7 +6,6 @@ import equipo1.registraduria.seguridad.Repositorios.RepositorioRol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
--- src/main/java/equipo1/registraduria/seguridad/Repositorios/RepositorioUsuario.java
+++ src/main/java/equipo1/registraduria/seguridad/Repositorios/RepositorioUsuario.java
@@ -1,6 +1,8 @@
 package equipo1.registraduria.seguridad.Repositorios;
-
-
-public interface RepositorioUsuario {
-
+import org.springframework.data.mongodb.repository.Query;
+import equipo1.registraduria.seguridad.Modelos.Usuario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+public interface RepositorioUsuario extends MongoRepository<Usuario,String>{
+   @Query("{'correo':?0}")
+   public Usuario getUserByEmail(String correo);
 }
